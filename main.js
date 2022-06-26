5027c593b56c63b1139c4a4850f772d5cdf30cc4
lucaslima <lucaslima@pop-os.localdomain>
2022-06-25 20:16:42 -0400

    DoctorCare

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
     if (scrollY > 0) {
         navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
     }
 }
+
+function showBackToTopButtonOnScroll() {
+    if (scrollY > 550) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
 
 function openMenu() {
     document.body.classList.add('menu-expanded')
